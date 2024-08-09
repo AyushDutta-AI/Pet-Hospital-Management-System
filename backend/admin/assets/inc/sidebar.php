f41d03987dbd370c679e361f6989135fee25eb05
Ayush Dutta <ayush980809@gmail.com>
2024-08-09 21:54:09 +0530

    Initial commit

---
+++ backend/admin/assets/inc/sidebar.php
@@ -0,0 +1,139 @@
+<div class="left-side-menu">
+
+                <div class="slimscroll-menu">
+
+                    <!--- Sidemenu -->
+                    <div id="sidebar-menu">
+
+                        <ul class="metismenu" id="side-menu">
+
+                            <li class="menu-title">Navigation</li>
+
+                            <li>
+                                <a href="his_admin_dashboard.php">
+                                    <i class="fe-airplay"></i>
+                                    <span> Dashboard </span>
+                                </a>
+                                
+                            </li>
+
+                            <li>
+                                <a href="javascript: void(0);">
+                                    <i class="fab fa-accessible-icon "></i>
+                                    <span> Pets </span>
+                                    <span class="menu-arrow"></span>
+                                </a>
+                                <ul class="nav-second-level" aria-expanded="false">
+                                    <li>
+                                        <a href="his_admin_register_patient.php">Register Pets</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_view_patients.php">View Pets</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_manage_patient.php">Manage Pets</a>
+                                    </li>
+                                    <hr>
+                                    
+                                </ul>
+                            </li>
+
+                            <li>
+                                <a href="javascript: void(0);">
+                                    <i class="mdi mdi-doctor"></i>
+                                    <span> Veterinarians </span>
+                                    <span class="menu-arrow"></span>
+                                </a>
+                                <ul class="nav-second-level" aria-expanded="false">
+                                    <li>
+                                        <a href="his_admin_add_employee.php">Add Veterinarians </a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_view_employee.php">View Veterinarians </a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_manage_employee.php">Manage  Veterinarians </a>
+                                    </li>
+                                    <hr>
+                                    
+                                </ul>
+                            </li>
+
+                            <li>
+                                <a href="javascript: void(0);">
+                                    <i class="mdi mdi-pill"></i>
+                                    <span> Pharmacy </span>
+                                    <span class="menu-arrow"></span>
+                                </a>
+                                <ul class="nav-second-level" aria-expanded="false">
+                                    <li>
+                                        <a href="his_admin_add_pharm_cat.php">Add Pharm Category</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_view_pharm_cat.php">View Pharm Category</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_manage_pharm_cat.php">Manage Pharm Category</a>
+                                    </li>
+                                    <hr>
+                                    
+                                </ul>
+                            </li>
+
+                            <li>
+                                <a href="javascript: void(0);">
+                                    <i class="mdi mdi-cash-multiple "></i>
+                                    <span> Accounting </span>
+                                    <span class="menu-arrow"></span>
+                                </a>
+                                <ul class="nav-second-level" aria-expanded="false">
+                                    <li>
+                                        <a href="his_admin_add_acc.payable.php">Add Acc. Payable</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_manage_acc_payable.php">Manage Acc. Payable</a>
+                                    </li>
+                                    <hr>
+                                    <li>
+                                        <a href="his_admin_add_acc_receivable.php">Add Acc. Receivable</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_manage_acc_receivable.php">Manage Acc. Receivable</a>
+                                    </li>
+                                    <hr>
+                                    
+                                    
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="javascript: void(0);">
+                                    <i class="fe-share"></i>
+                                    <span> Reporting </span>
+                                    <span class="menu-arrow"></span>
+                                </a>
+                                <ul class="nav-second-level" aria-expanded="false">
+                                    
+                                    <li>
+                                        <a href="his_admin_employee_records.php">Veterinarians Records</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_pharmaceutical_records.php">Pharmaceutical Records</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_accounting_records.php">Accounting Records</a>
+                                    </li>
+                                    
+                                    
+                                </ul>
+                            </li>
+                        </ul>
+
+                    </div>
+                    <!-- End Sidebar -->
+
+                    <div class="clearfix"></div>
+
+                </div>
+                <!-- Sidebar -left -->
+
+            </div>
---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required Meta Tags -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Page Title -->
+    <title>Pet Healthcare Management System</title>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="assets/images/logo/favicon.png" type="image/x-icon">
+
+    <!-- CSS Files -->
+    <link rel="stylesheet" href="assets/css/animate-3.7.0.css">
+    <link rel="stylesheet" href="assets/css/font-awesome-4.7.0.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap-4.1.3.min.css">
+    <link rel="stylesheet" href="assets/css/owl-carousel.min.css">
+    <link rel="stylesheet" href="assets/css/jquery.datetimepicker.min.css">
+    <link rel="stylesheet" href="assets/css/linearicons.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <!-- Preloader Starts -->
+    <div class="preloader">
+        <div class="spinner"></div>
+    </div>
+    <!-- Preloader End -->
+
+    <!-- Header Area Starts -->
+    <header class="header-area">
+        <div id="header" id="home">
+            <div class="container">
+                <div class="row align-items-center justify-content-between d-flex">
+                <div id="logo">
+                    <a href="index.php"></a>
+                </div>
+                <nav id="nav-menu-container">
+                    <ul class="nav-menu">
+                        <li><a href="backend/admin/index.php">Login to access the database</a></li>
+                    </ul>
+                </nav><!-- #nav-menu-container -->		    		
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- Header Area End -->
+
+    <!-- Banner Area Starts -->
+    <section class="banner-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8">
+                    <h4>Healthy pets, happy hearts</h4>
+                    <h1>"Empowering Pet Health, One Click at a Time."</h1>
+                    <p>Streamline pet healthcare with our user-friendly online platform. From appointments to medical records, we make managing your pet's well-being effortless and accessible. Join us in nurturing happier, healthier pets!</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+   
+
+
+    <!-- Javascript -->
+    <script src="assets/js/vendor/jquery-2.2.4.min.js"></script>
+	<script src="assets/js/vendor/bootstrap-4.1.3.min.js"></script>
+    <script src="assets/js/vendor/wow.min.js"></script>
+    <script src="assets/js/vendor/owl-carousel.min.js"></script>
+    <script src="assets/js/vendor/jquery.datetimepicker.full.min.js"></script>
+    <script src="assets/js/vendor/jquery.nice-select.min.js"></script>
+    <script src="assets/js/vendor/superfish.min.js"></script>
+    <script src="assets/js/main.js"></script>
+
+</body>
+</html>
